432bd009f77a9d5b9fb13ecd4856e9ad1a5d9ee6
stuckatmarine <mwd467@mun.ca>
2018-06-28 16:16:19 -0230

    increased runtime efficiency
    added collider boxes, baked lighting, added physics cast to target. Partial implemented target list

--- Assets/kartBoi/Scripts/kartController.cs
+++ Assets/kartBoi/Scripts/kartController.cs
@@ -5,13 +5,28 @@ using UnityEngine.UI;
 
 public class kartController : MonoBehaviour {
 
-
+    // Gui textbox objects
     public Text xLocationText;
     public Text zLocationText;
     public Text distToTargetText;
 
+    // Class for each target to be reached
+ /*   public class Target
+    {
+        public GameObject GameObject;
+        public Transform transform;
+        public bool found = false;
+        public bool seen = false;
+        public float distance;
+    }
+    public List<Target> targetList; // list of targets to be reached
+*/
     public Transform currentTarget; //position of target
+    public bool targetFound = false;
     public float distToTarget; //distance to target (x & y)
+
+    public int currentTargetIndex; // List index of current target
+ //   public Target exit; // Target after all other targets are found
 
     public bool objectForward = false; //detect if objects around cart
     public bool objectBehind = false;
@@ -23,7 +38,8 @@ public class kartController : MonoBehaviour {
         xLocationText.text = (transform.position.x).ToString();
         zLocationText.text = (transform.position.z).ToString();
         UpdateDistToTarget();
-        CheckForObstacles();
+        //CheckForObstacles();
+     //   exit.GameObject = GameObject.Find("Exit"); 
     }
 	
 	// Update is called once per frame
@@ -52,7 +68,7 @@ public class kartController : MonoBehaviour {
             this.transform.position = new Vector3((int)(this.transform.position.x + 0.5) + amount, this.transform.position.y, this.transform.position.z);
             xLocationText.text = ((int)transform.position.x).ToString();
             UpdateDistToTarget();
-            CheckForObstacles();
+           // CheckForObstacles();
         }
     }
 
@@ -71,7 +87,6 @@ public class kartController : MonoBehaviour {
             this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, (int)(this.transform.position.z + 0.5) + amount);
             zLocationText.text = ((int)transform.position.z).ToString();
             UpdateDistToTarget();
-            CheckForObstacles();
         }
     }
 
@@ -79,13 +94,21 @@ public class kartController : MonoBehaviour {
     {
         distToTarget = Vector3.Distance(currentTarget.position, transform.position);
         distToTargetText.text = distToTarget.ToString();
+        LookForTarget();
     }
 
     private void CheckForObstacles()
     {
-    /*    objectRight = Physics.Linecast(transform.position, new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z));
+        objectRight = Physics.Linecast(transform.position, new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z));
         objectLeft = Physics.Linecast(transform.position, new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z));
         objectForward = Physics.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f));
         objectBehind = Physics.Linecast(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z - 1f));
-    */}
+    }
+
+    // raycast from kart to target to confirm a line of sign and travel towards
+    private void LookForTarget()
+    {
+        //requires check for tag
+        targetFound = Physics.Linecast(transform.position, new Vector3(currentTarget.position.x, currentTarget.position.y, currentTarget.position.z));
+    }
 }
